1f68fd2dfbcb9b5d7a3a2dd3988b6f127cf5da3a
Nikhil <nikhil.kapoor@rsystems.com>
2018-06-07 17:47:10 +0530

    Filter for Name

---
+++ filter.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'Filter',
+})
+export class Filter implements PipeTransform {
+
+  transform(input: any, args: any, cloumn: any): any {
+    debugger;
+    const data = [];
+    if (!input || !args) {
+      return input;
+    }
+    input
+    // filter items array, items which match and return true will be kept, false will be filtered out
+    input.forEach(item => {
+      if(item[cloumn].toLowerCase().search(args.toLowerCase()) !== -1){
+        data.push(item);
+      }
+    });
+    return data;
+  }
+}
+
--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -4,15 +4,18 @@ import { routes, routing } from './employee.routing'
 import { EmployeeTable } from "./employee-table/employee.component";
 import { PagesComponent } from "./employee.component";
 import { CommonModule } from "@angular/common";
+import { Filter } from "../../filter.pipe";
 
 @NgModule( {
     imports: [
       routing,
       CommonModule,
+      FormsModule,
     ],
     declarations: [
       PagesComponent,
       EmployeeTable,
+      Filter,
     ],
 } )
 
